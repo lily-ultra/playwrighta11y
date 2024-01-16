900ced94c0f42c11ea8c18215ea757c6475e917e
Lily May <lmay@ultranauts.co>
2024-01-16 13:01:57 -0500

    test fixes

--- tests/alt.test.js
+++ tests/alt.test.js
@@ -1,24 +1,19 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('alt text 1.1.1', async ({ page }) => {
+test('alt text 1.1.1a', async ({ page }) => {
 
   await page.goto('https://sussexcountyde.gov/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Sussex County/);
-
+  
   // Get all img tags on the page
   const imgTags = await page.$$('img');
 
   for (const imgTag of imgTags) {
     const altText = await imgTag.getAttribute('alt');
-
     if (!altText) {
       console.log('Image without Alt text found! ' + imgTag );
     }
-    await expect(altText !== '');
-    
-  }
 
+    await expect(altText).not.toBe('')    
+  }
 });
